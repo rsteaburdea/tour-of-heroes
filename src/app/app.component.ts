4f9366be622891f7a1f104861128d561cfeebaad
rsteaburdea <robert.steaburdea@gmail.com>
2021-06-06 12:32:56 +0300

    The Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+// The page you see is the application shell. The shell is controlled by an Angular component named AppComponent
 export class AppComponent {
   title = 'Tour of Heroes';
 }
